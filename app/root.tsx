a9d03e49c3401ca47ffda4e21d8eb134d34308e0
Nikos Douvlis <nikosdouvlis@gmail.com>
2022-02-25 23:03:23 +0200

    Replace ClerkProvider with ConnectClerk in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,7 +1,7 @@
 import type { MetaFunction } from "remix";
 import { Links, LiveReload, LoaderFunction, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "remix";
-import { ClerkProvider } from "@clerk/remix";
-import { rootAuthLoader, WithClerkState } from "@clerk/remix/ssr.server";
+import { ConnectClerk } from "@clerk/remix";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
 import { VisualWrapper } from "~/components/VisualWrapper";
 import { HeaderLinks } from "~/components/HeaderLinks";
 
@@ -12,40 +12,38 @@ export const meta: MetaFunction = () => {
 export const loader: LoaderFunction = (args) =>
   rootAuthLoader(
     args,
-    ({ auth, user }) => {
+    ({ auth }) => {
       const { userId, sessionId, getToken } = auth;
-      console.log("rootAuthLoader auth", { userId, sessionId, getToken });
-      console.log("rootAuthLoader user", user);
-      return `Hello from the root loader :)`;
+      console.log("rootAuthLoader auth:", { userId, sessionId, getToken });
+      return { message: `Hello from the root loader :)` };
     },
     { loadUser: true }
   );
 
-export default function App() {
-  const { data, clerkState } = useLoaderData<WithClerkState<string>>();
+function App() {
+  const { message } = useLoaderData();
 
   return (
-    <ClerkProvider frontendApi={"clerk.renewing.hermit-14.lcl.dev"} clerkState={clerkState}>
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width,initial-scale=1" />
-          <Meta />
-          <Links />
-        </head>
-        <body>
-          <VisualWrapper color={"green"}>
-            <h1>Index (root) route</h1>
-            <HeaderLinks />
-            <h2>Data returned from root loader: </h2>
-            <pre>{data}</pre>
-            <Outlet />
-          </VisualWrapper>
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <VisualWrapper color={"green"}>
+          <h1>Index (root) route</h1>
+          <HeaderLinks />
+          <h2>Data returned from root loader: </h2>
+          <pre>{message}</pre>
+          <Outlet />
+        </VisualWrapper>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
   );
 }
+export default ConnectClerk(App, { frontendApi: "clerk.renewing.hermit-14.lcl.dev" });
